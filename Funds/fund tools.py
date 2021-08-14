e3c5bac3e201ef6e270da6d4db79f5cc14182894
Sean <wshuyi747@gmail.com>
2021-08-14 22:32:55 +0800

    8-14-2232

--- Funds/fund tools.py
+++ Funds/fund tools.py
@@ -2,10 +2,17 @@ import numpy as np
 import pandas as pd
 import akshare as ak
 
-def get_fund_categories():
+def get_fund_categories(open_fund=False):
 
     fund_em_fund_name_df = ak.fund_em_fund_name()
-    fund_categories = np.unique(fund_em_fund_name_df['基金类型'].values)
+
+    if open_fund:
+        fund_em_open_fund_daily_df = ak.fund_em_open_fund_daily()
+        df = pd.merge(fund_em_open_fund_daily_df, fund_em_fund_name_df, on='基金代码')
+
+        fund_categories = np.unique(df['基金类型'].values)
+    else:
+        fund_categories = np.unique(fund_em_fund_name_df['基金类型'].values)
 
     return fund_categories
 
@@ -30,4 +37,18 @@ def get_fund_net_worth(fund_code, start_date, end_date):
     df = df.loc[mask].reset_index().drop('index', axis=1)
 
     return df
+
+def get_fund_cumulative_return(fund_code, start_date, end_date):
+
+    start_date = pd.to_datetime(start_date, format='%Y/%m/%d')
+    end_date = pd.to_datetime(end_date, format='%Y/%m/%d')
+
+    df = ak.fund_em_open_fund_info(fund=fund_code, indicator="累计收益率走势")
+    mask = (df['净值日期'] > start_date) & (df['净值日期'] <= end_date)
+    df = df.loc[mask].reset_index().drop('index', axis=1)
+
+    return df
+
+
+
 
