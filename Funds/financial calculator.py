fc31e88277e21c0a8cf8f5a51279094a5b4dde38
Sean <wshuyi747@gmail.com>
2021-10-03 10:59:43 +0800

    Create financial calculator.py

---
+++ Funds/financial calculator.py
@@ -0,0 +1,9 @@
+import numpy as np
+import math
+
+def annualizer(daliy_return, investment_period):
+
+    annualized_return = daliy_return ** (365/investment_period) - 1
+
+    return annualized_return
+
